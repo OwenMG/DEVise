02c880f1cfa26612ab0cb4445aad210f9a3691d8
Christina R <xsnachannell@gmail.com>
2022-06-01 21:14:47 -0700

    updates login.js to include query selector and event listeners to handle sign up and login form submits

--- public/js/login.js
+++ public/js/login.js
@@ -5,9 +5,19 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#email-login').value.trim();
 
     if (username && password) {
-        const response = await fetch('/api/users/login')
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
     }
-}
+};
 
 const signupFormHandler = async (event) => {
     event.preventDefault();
@@ -15,6 +25,23 @@ const signupFormHandler = async (event) => {
     const email = document.querySelector('#email-login').value.trim();
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-    const firstName = document.querySelector('#firstName-login').value.trim();
-    const firstName = document.querySelector('#firstName-login').value.trim();
-}
+    const firstName = document.querySelector('#firstName-login').value.toLowerCase().trim();
+    const lastName = document.querySelector('#lastName-login').value.toLowerCase().trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/', {
+            method: 'POST',
+            body: JSON.stringify({ email, username, password, firstName, lastName }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);

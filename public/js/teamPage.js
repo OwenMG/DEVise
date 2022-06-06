cae267cfdbef8eb4490b324b899a247183391629
Christina R <xsnachannell@gmail.com>
2022-06-05 20:31:02 -0700

    updates teamPage.js so that public side accesses routes to create a new team and have user join a team

--- public/js/teamPage.js
+++ public/js/teamPage.js
@@ -1,14 +1,44 @@
-const teamPage = async () => {
-    const response = await fetch('/api/users/logout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-    });
+const createTeamFormHandler = async (event) => {
+    event.preventDefault();
 
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert(response.statusText); //sends alert box in browser to user with error message that was coded on back end
+    const name = document.querySelector('#team-name').value.trim();
+    const password = document.querySelector('#team-password').value.trim();
+
+    if (teamName && password) {
+        const response = await fetch('/api/teams/', {
+            method: 'POST',
+            body: JSON.stringify({ name, password }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
     }
 };
 
-document.querySelector('#login_button');
+const joinTeamFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#team-name').value.trim();
+    const password = document.querySelector('#team-password').value.trim();
+
+    if (teamName && password) {
+        const response = await fetch('/api/teams/jointeam', {
+            method: 'POST',
+            body: JSON.stringify({ name, password }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('.join-form').addEventListener('submit', joinTeamFormHandler);
+document.querySelector('.create-form').addEventListener('submit', createTeamFormHandler);

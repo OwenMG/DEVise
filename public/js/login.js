39996dfeabd6b495fa2cef86ea579733b884b4ae
Christina R <xsnachannell@gmail.com>
2022-06-06 19:27:13 -0700

    updates id name in login.js

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     const username = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
     if (username && password) {
         const response = await fetch('/api/users/login', {

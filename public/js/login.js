547c0db347e4312da4eba9948c0b1ede84f5b783
Christina R <xsnachannell@gmail.com>
2022-06-06 20:09:51 -0700

    updates login with correct ids to connect with html form input

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,8 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
 
-    const username = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#typeEmailX').value.trim();
+    const password = document.querySelector('#typePasswordX').value.trim();
 
     if (username && password) {
         const response = await fetch('/api/users/login', {
@@ -21,4 +21,4 @@ const loginFormHandler = async (event) => {
 
 
 
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.btn').addEventListener('submit', loginFormHandler);

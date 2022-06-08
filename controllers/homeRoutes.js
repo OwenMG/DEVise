2f462705991a2a3ed6766fcccb3848ce7f7a8b62
Christina R <xsnachannell@gmail.com>
2022-06-07 21:04:39 -0700

    updates homeRoutes to include routes to access team page and team dashboard and adds js script to joinTeam handlebars view

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const Authenticated = require('../utils/auth');
 
 //will need to display log in or sign in options
 router.get('/', async (req, res) => {
     if(req.session.logged_in) {
-        res.redirect('/teamDash');
+        res.redirect('/joinTeam');
         return;
     }
     res.render('login');
@@ -11,19 +12,20 @@ router.get('/', async (req, res) => {
 
 router.get('/signup', async (req, res) => {
     if(req.session.logged_in) {
-        res.redirect('/teamDash');
+        res.redirect('/joinTeam');
         return;
     }
     res.render('signup');
 });
 
-router.get('/joinTeam', async (req, res) => {
-    if(req.session.logged_in) {
-        res.redirect('/teamDash');
-        return;
-    }
+router.get('/joinTeam', Authenticated, async (req, res) => {
+
     res.render('joinTeam');
 });
+
+router.get('/teamDash', Authenticated, async (req, res) => {
+    res.render('teamDash');
+})
 
 
 module.exports = router;
